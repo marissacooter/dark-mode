6fc757999d6681f12e7bff10610fb2fd9254aa90
Marissa <marissacooterl@gmail.com>
2020-04-08 15:22:28 -0500

    setting up dark mode functions

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import useLocalStorage from './useLocalStorage';
+import {useEffect} from 'react';
+
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage('dark-mode')
+}
