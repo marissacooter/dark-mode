f76248bed6a21f18b1c1a92e2443282fc0ea630d
Marissa <marissacooterl@gmail.com>
2020-04-08 15:35:54 -0500

    editing dark mode functions

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,4 +3,12 @@ import {useEffect} from 'react';
 
 export const useDarkMode = () => {
     const [darkMode, setDarkMode] = useLocalStorage('dark-mode')
+
+    useEffect(() => {
+        if (darkMode === true ){
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    },[darkMode])
 }
